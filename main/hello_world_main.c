eb98da37b4fa89ec79f8307179257e58e010daa8
Aidan Pinard <aidan@aidanpinard.co>
2022-10-04 16:05:17 -0400

    Modified main to have a static constant for reset time.
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -6,37 +6,36 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-#include <stdio.h>
+#include "esp_spi_flash.h"
+#include "esp_system.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
+#include <stdio.h>
 
 static const uint32_t STUDENT_ID = 816027279;
-static const char* STUDENT_NAME = "Aidan Pinard";
+static const char *STUDENT_NAME = "Aidan Pinard";
+static const int last_two_digits = ((STUDENT_ID) % 100) % 17;
 
-void app_main()
-{
+void app_main() {
     printf("%s - %d\n", STUDENT_NAME, STUDENT_ID);
 
     /* Print chip information */
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
-    printf("This is ESP8266 chip with %d CPU cores, WiFi, ",
-            chip_info.cores);
+
+    printf("This is ESP8266 chip with %d CPU cores, WiFi, ", chip_info.cores);
 
     printf("silicon revision %d, ", chip_info.revision);
 
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    // Get the last two digits for the number of seconds to restart
-    const int last_two_digits = ((STUDENT_ID) % 100) % 17; 
-
+           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded"
+                                                       : "external");
+  
     for (int i = last_two_digits; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+      printf("Restarting in %d seconds...\n", i);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+
     printf("Restarting now.\n");
     fflush(stdout);
     esp_restart();
